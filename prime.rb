d53d1c693d892109e7ed8d2dd0f191dc4d0e71da
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 22:37:07 +0000

    Automatically backed up by Learn

---
+++ prime.rb
@@ -0,0 +1,15 @@
+
+def is_prime(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+    puts n 
+    puts num 
+  end
+  true
+end
+
+puts is_prime(7)
+#=> true
+
+# puts is_prime(4)
+#=> false

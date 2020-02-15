5817084cc6117fa928fe34ad19d4197cda574021
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 22:53:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+array = [7,13,19,15]
 def is_prime(array)
 
 array.each do |num|
@@ -5,9 +6,12 @@ array.each do |num|
 
   (2..(num - 1)).each do |n|
     return false if num % n == 0
+    puts array
     puts n 
     puts num 
   end
   true
 end
 end
+
+puts is_prime(array)

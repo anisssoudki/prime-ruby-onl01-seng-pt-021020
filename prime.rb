f98726aa6ba79e79bb28825515e84fbf4ec255cd
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 20:38:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,21 @@
 
 def prime?(integer) 
   
+  i = 0 
   
+  while i < integer.length 
+
+    number % number == 1
+  
+  i += 1 
+  
+  if number % number   
+    return true 
+    
+    elsif number == 1 
+    false
+  else 
+    false
   
   
 end 

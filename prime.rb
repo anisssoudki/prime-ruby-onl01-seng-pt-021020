233b850b5c99cc0ab1093c0af924feef3d40ea21
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 23:30:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,17 @@
-def is_prime(num)
-  (2..(num - 1)).each do |n|
-    puts false if num % n == 0
-  end
- puts true
-end
+# def is_prime(num)
+#   (2..(num - 1)).each do |n|
+#     puts false if num % n == 0
+#   end
+# puts true
+# end
 
 
-def prime?(array)
+# def prime?(array)
 
-array.each do |item| 
-is_prime(item)
-puts item
-end
-end
+# array.each do |item| 
+# is_prime(item)
+# puts item
+# end
+# end
 
-array= [2,-1,165464]
+# array= [2,-1,165464]

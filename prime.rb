6151c5323183a91a563e415df07f47f108993494
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 21:23:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,39 @@
+require 'benchmark'
+require 'bigdecimal/math'
 
-def prime?(integer) 
-  
-  i = 0 
-  
-  while i < integer.length 
+# calculate pi to 10k digits
+puts Benchmark.measure { BigMath.PI(10_000) }
 
-    number % number == 1
-  
-  i += 1 
-  
-  if number % number   
-    return true 
-    
-    elsif number == 1 
-    false
-  else 
-    false
-  
-  
-end 
+def sieve(max)
+  # Set up an array with all the numbers from 0 to the max
+  primes = (0..max).to_a
+
+  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
+  # aren't prime.
+  primes[0] = primes[1] = nil
+
+  # Iterate through primes array
+  counter = 0
+  primes.each do |p|
+    # Skip if nil
+    next unless p
+
+    # Break if we are past the square root of the max value 
+    break if p*p > max
+    counter += 1
+    # Start at the square of the current number, and step through.
+    # Go up to the max value, by multiples of the current number, and replace
+    # that value with nil in the primes array
+    (p*p).step(max,p) { |m| primes[m] = nil }
+  end
+
+  # Finally, return the compacted array.
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
+end
+
+def prime?(num)
+  sieve(num).include?(num)
+end
+
+puts prime?(105557)
